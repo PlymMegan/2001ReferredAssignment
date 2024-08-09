09b16afcbea4cf951ed1d92af4397cee9bd1b5da
Megan White <megan.white-14@students.plymouth.ac.uk>
2024-08-09 14:14:51 +0100

    created dbcontext

---
+++ ModerationMicroservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class ApplicationDbContext : DbContext
+{
+    // Define your tables as DbSets
+    public DbSet<YourEntity> YourEntities { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Configure the model with Fluent API if needed
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class YourEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    // Other properties
+}
