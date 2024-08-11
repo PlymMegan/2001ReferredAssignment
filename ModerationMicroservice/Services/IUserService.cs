dba958d2855cbeb439f8634e23f28657344a3d9c
Megan White <megan.white-14@students.plymouth.ac.uk>
2024-08-11 15:51:59 +0100

    added services

---
+++ ModerationMicroservice/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using ModerationMicroservice.Entities;
+
+namespace ModerationMicroservice.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserById(int id);
+        Task CreateUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(int id);
+    }
+}
---
+++ ModerationMicroservice/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using ModerationMicroservice.Entities;
+using ModerationMicroservice.Repositories;
+
+namespace ModerationMicroservice.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _userRepository.GetAllUsers();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await _userRepository.GetUserById(id);
+        }
+
+        public async Task CreateUser(User user)
+        {
+            await _userRepository.AddUser(user);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            await _userRepository.UpdateUser(user);
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            await _userRepository.DeleteUser(id);
+        }
+    }
+
+}
