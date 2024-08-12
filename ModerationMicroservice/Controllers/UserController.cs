46b589f4aae7233a5134ebc9f2880d78af1b0f00
Megan White <megan.white-14@students.plymouth.ac.uk>
2024-08-12 13:56:54 +0100

    added controller

---
+++ ModerationMicroservice/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModerationMicroservice.Entities;
+using ModerationMicroservice.Services;
+
+namespace ModerationMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetUserById(id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            await _userService.CreateUser(user);
+            return CreatedAtAction(nameof(GetUserById), new { id = user.UserID }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.UserID)
+                return BadRequest();
+
+            await _userService.UpdateUser(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            await _userService.DeleteUser(id);
+            return NoContent();
+        }
+    }
+
+}
